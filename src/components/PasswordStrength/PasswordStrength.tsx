2a19a470185aee24219131e02fe77ed8e68baeed
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-12-21 07:34:34 -0600

    style the password strenth bar, displays proper colors based on password strength

--- src/components/PasswordStrength/PasswordStrength.tsx
+++ src/components/PasswordStrength/PasswordStrength.tsx
@@ -1,18 +1,32 @@
+import styles from './PasswordStrength.module.css';
+
 interface PasswordStrengthProps {
   passwordScoreNum: number;
 }
 
 const PasswordStrength = ({ passwordScoreNum }: PasswordStrengthProps) => {
   return (
-    <div>
-      <h3>STRENGTH</h3>
-      <div>
-        <p data-cy="passwordStrengthText">
+    <div className={styles.passwordStrength}>
+      <h4>STRENGTH</h4>
+      <div className={styles.meterContainer}>
+        <p
+          data-cy="passwordStrengthText"
+          className={styles.passwordStrengthText}
+        >
           {passwordScoreNum === 1 ? 'TOO WEAK!' : null}
           {passwordScoreNum === 2 ? 'WEAK!' : null}
           {passwordScoreNum === 3 ? 'MEDIUM' : null}
           {passwordScoreNum === 4 ? 'STRONG' : null}
         </p>
+        <div
+          className={styles.passwordStrengthMeter}
+          data-strength={passwordScoreNum}
+        >
+          <div className={styles.meterBarOne}></div>
+          <div className={styles.meterBarTwo}></div>
+          <div className={styles.meterBarThree}></div>
+          <div className={styles.meterBarFour}></div>
+        </div>
       </div>
     </div>
   );
