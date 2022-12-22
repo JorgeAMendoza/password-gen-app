7c33fe6cff963c788782669b7a8fda9448a78ef9
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-12-22 10:47:36 -0600

    use the pixel perfect extension on chrome to adjust the mobile design to get it as close as possible to the figma prototype

--- src/components/PasswordDisplay/PasswordDisplay.tsx
+++ src/components/PasswordDisplay/PasswordDisplay.tsx
@@ -1,5 +1,5 @@
 import copyIcon from '../../assets/icon-copy.svg';
-import style from './PasswordDisplay.module.css';
+import styles from './PasswordDisplay.module.css';
 
 interface PasswordDisplayProps {
   password: string;
@@ -8,8 +8,8 @@ interface PasswordDisplayProps {
 const PasswordDisplay = ({ password }: PasswordDisplayProps) => {
   const copyPassword = () => navigator.clipboard.writeText(password);
   return (
-    <section className={style.passwordDisplay}>
-      <h2 data-cy="password" className={style.passwordText}>
+    <section className={styles.passwordDisplay}>
+      <h2 data-cy="password" className={styles.passwordText}>
         {password}
       </h2>
       <button
@@ -18,7 +18,11 @@ const PasswordDisplay = ({ password }: PasswordDisplayProps) => {
         aria-label="Copy generated password to clipboard"
         data-cy="copyPasswordButton"
       >
-        <img src={copyIcon} alt="copy to clipboard icon" />
+        <img
+          src={copyIcon}
+          alt="copy to clipboard icon"
+          className={styles.copyImage}
+        />
       </button>
     </section>
   );
